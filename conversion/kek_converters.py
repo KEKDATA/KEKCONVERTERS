d8a0b5591b7cbd3fa19d6ae301310af1fbc7d4c6
Xevolesi <easytomalbolge@gmail.com>
2020-03-29 16:19:04 +0300

    Add pascalvoc2kek and kek2pascalvoc conversion

--- conversion/kek_converters.py
+++ conversion/kek_converters.py
@@ -1,6 +1,9 @@
 """This module contains converter's classes."""
 import os
-from typing import Union, Dict
+import xml.etree.ElementTree as ET
+from typing import Union, Dict, List
+from xml.dom.minidom import parseString
+
 
 from PIL import Image
 
@@ -33,8 +36,7 @@ class ToKEKFormatConverter(BaseConverter):
         Converts Darknet annotation format for given image to KEKFormat representation.
 
         :param image: Source image;
-        :param base_annotation_path: Path to directory which contains .txt annotation
-                                     files.
+        :param base_annotation_path: Path to directory which contains .txt annotation files.
 
         :return: KEKFormat representation.
         """
@@ -64,8 +66,49 @@ class ToKEKFormatConverter(BaseConverter):
             )
         return KEKFormat(kek_objects, image_metadata)
 
-    def _from_pascal_voc(self) -> KEKFormat:
-        pass
+    def _from_pascal_voc(self, image: os.DirEntry, base_annotation_path: str = None) -> KEKFormat:
+        """
+        Converts PASCAL VOC annotation format for given image to KEKFormat representation.
+
+        :param image: Source image;
+        :param base_annotation_path: Path to directory which contains .xml annotation files.
+
+        :return: KEKFormat representation.
+        """
+        image_name, image_ext = os.path.splitext(image.name)
+        if not base_annotation_path:
+            base_xml_path = os.path.split(image.path)[0]
+        else:
+            base_xml_path = base_annotation_path
+        xml_name = '.'.join([image_name, 'xml'])
+        xml_path = os.path.join(base_xml_path, xml_name)
+        root = ET.parse(xml_path)
+        image_metadata = {
+            'folder': root.find('folder').text,
+            'filename': root.find('filename').text,
+            'path': root.find('path').text,
+            'database': root.find('source').find('database').text,
+            'image_width': int(root.find('size').find('width').text),
+            'image_height': int(root.find('size').find('height').text),
+            'image_depth': int(root.find('size').find('depth').text),
+            'segmented': int(root.find('segmented').text)
+        }
+        objects = root.findall('object')
+        kek_objects = []
+        for object_ in objects:
+            class_name = object_.find('name').text
+            class_id = self.class_mapper[class_name]
+            pv_metadata = {
+                'pose': object_.find('pose').text,
+                'truncated': object_.find('truncated').text,
+                'difficult': object_.find('difficult').text
+            }
+            box = [int(object_.find('bndbox').find('xmin').text), int(object_.find('bndbox').find('ymin').text),
+                   int(object_.find('bndbox').find('xmax').text), int(object_.find('bndbox').find('ymax').text)]
+            kek_objects.append(
+                KEKObject(class_id, class_name, KEKBox(box), pv_metadata)
+            )
+        return KEKFormat(kek_objects, image_metadata)
 
     def _from_ms_coco(self) -> KEKFormat:
         pass
@@ -78,12 +121,56 @@ class ToKEKFormatConverter(BaseConverter):
         }.get(self.annotation, self.raise_not_implemented)(*args, **kwargs)
 
 
-class FromKEKFormatCOnverter(BaseConverter):
-    def _to_darknet(self):
-        pass
+class FromKEKFormatConverter(BaseConverter):
+    def __init__(self, annotation: str):
+        super().__init__(annotation)
 
-    def _to_pascal_voc(self):
-        pass
+    def _to_darknet(self, kek_format: KEKFormat) -> List[str]:
+        darknet_labels = []
+        image_width = kek_format.image_metadata['image_width']
+        image_height = kek_format.image_metadata['image_height']
+        image_depth = kek_format.image_metadata['image_depth']
+        image_shape = (image_height, image_width, image_depth)
+        for kek_object in kek_format.kek_objects:
+            class_id = str(kek_object.class_id)
+            darknet_box = kek_object.kek_box.to_darknet_box(image_shape)
+            darknet_labels.append(
+                ' '.join([class_id, *map(str, darknet_box)]) + '\n'
+            )
+        return darknet_labels
+
+    def _to_pascal_voc(self, kek_format: KEKFormat) -> str:
+        root = ET.Element('annotation')
+        for k, v in kek_format.image_metadata.items():
+            if k == 'database':
+                src = ET.SubElement(root, 'source')
+                database = ET.SubElement(src, 'database')
+                database.text = v
+            elif k in ('image_width', 'image_height', 'image_depth'):
+                if not root.find('size'):
+                    size = ET.SubElement(root, 'size')
+                sub = ET.SubElement(size, k.split('_')[-1])
+                sub.text = str(v)
+            else:
+                sub = ET.SubElement(root, k)
+                sub.text = str(v)
+        for kek_object in kek_format.kek_objects:
+            object_ = ET.SubElement(root, 'object')
+            sub = ET.SubElement(object_, 'name')
+            sub.text = kek_object.class_name
+            for k, v in kek_object.pv_metadata.items():
+                sub = ET.SubElement(object_, k)
+                sub.text = str(v)
+            bndbox = ET.SubElement(object_, 'bndbox')
+            kek_box = [kek_object.kek_box.top_left_x, kek_object.kek_box.top_left_y,
+                       kek_object.kek_box.bottom_right_x, kek_object.kek_box.bottom_right_y]
+            for tag, value in zip(('xmin', 'ymin', 'xmax', 'ymax'), kek_box):
+                sub = ET.SubElement(bndbox, tag)
+                sub.text = str(value)
+        xml_string = parseString(ET.tostring(root)).toprettyxml()
+        # First line would be '<?xml version="1.0"?>' but it's not needed.
+        without_declaration = '\n'.join(xml_string.split('\n')[1:])
+        return without_declaration
 
     def _to_ms_coco(self):
         pass
